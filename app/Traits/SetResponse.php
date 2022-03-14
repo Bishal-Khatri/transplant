28b148a173aabc128139b5579dcee81f713031a1
Bishal Khatri <bishal.khatri343@gmail.com>
2022-03-14 20:54:49 +0545

    return object on empty data in API

--- app/Traits/SetResponse.php
+++ app/Traits/SetResponse.php
@@ -1,10 +1,15 @@
 <?php
 namespace App\Traits;
 
+use stdClass;
+
 trait SetResponse
 {
     public function prepareResponse($error, $message, Array $data, Array $meta)
     {
+        if(is_array($data) AND blank($data)){
+            $data = new stdClass();
+        }
         return [
             'error' => $error,
             'message' => $message,

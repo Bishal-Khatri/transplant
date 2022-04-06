cd0002d0d99689a7f27d829c502ed5123d3b3c4a
Bishal Khatri <bishal.khatri343@gmail.com>
2022-04-06 23:51:44 +0545

    fix

--- Modules/User/Http/Controllers/UserController.php
+++ Modules/User/Http/Controllers/UserController.php
@@ -24,7 +24,7 @@ class UserController extends Controller
     public function userListing(Request $request)
     {
         $user_type = UserType::SUBSCRIBER;
-        if ($request->has('user_type')){
+        if ($request->has('user_type') AND !blank($request->user_type)){
             $user_type = $request->user_type;
         }
 

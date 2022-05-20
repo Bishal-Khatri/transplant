baef52432df59372b4a7fcbc2b107d81bd8425bb
prolaxu <adhikarilaxu0001@gmail.com>
2022-05-20 12:26:15 +0545

    Create PublicService.js

---
+++ Modules/ContentManagement/Resources/assets/services/PublicService.js
@@ -0,0 +1,16 @@
+import Api from './Api';
+
+export default {
+    getProvince(){
+        return Api().get('/web-api/public/province');
+    },
+    getDistrict(province_id){
+        return Api().get('/web-api/public/district'+'?province_id='+province_id);
+    },
+    getMunicipality(district_id){
+        return Api().get('/web-api/public/municipality'+'?district_id='+district_id);
+    },
+    getPalika(district_id){
+        return Api().get('/web-api/public/palika'+'?district_id='+district_id);
+    }
+}

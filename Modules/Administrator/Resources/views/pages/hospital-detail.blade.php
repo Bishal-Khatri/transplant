8390caaf897647de3d54841c8fc68aad455ff7e7
prolaxu <adhikarilaxu0001@gmail.com>
2022-05-24 23:58:49 +0545

    Create hospital-detail.blade.php

---
+++ Modules/Administrator/Resources/views/pages/hospital-detail.blade.php
@@ -0,0 +1,15 @@
+@extends('administrator::layouts.master')
+
+@section('content')
+    <div class="right_col" role="main">
+        <div class="">
+            <div class="page-title">
+                <div class="title_left">
+                    <h3>Hospital Listing</h3>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+            <hospital-details  hospital_json="{{ $hospital }}"/>
+        </div>
+    </div>
+@endsection

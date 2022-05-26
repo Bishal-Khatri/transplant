0620ac07bf7af4399fe57a49eda31a84b7b9afe9
prolaxu <adhikarilaxu0001@gmail.com>
2022-05-26 13:39:31 +0545

    Update PatientController.php

--- Modules/Hospital/Http/Controllers/PatientController.php
+++ Modules/Hospital/Http/Controllers/PatientController.php
@@ -30,6 +30,7 @@ class PatientController extends Controller
 
     public function savePatient(Request $request)
     {
+
         $request->validate([
             'patient_image' => 'required',
             'patient_name' => 'required',
@@ -55,7 +56,7 @@ class PatientController extends Controller
 
     public function updatePatient(Request $request)
     {
-//        dd($request->all());
+    //    dd($request->all());
         if (!isset($request->page) OR blank($request->page)){
             return;
         }
@@ -66,9 +67,22 @@ class PatientController extends Controller
 
         $patient = Patient::findOrFail($request->patient_id);
 
+        // dd($request->page);
         switch ($request->page){
             case 'personal_information':
                 return $this->__updatePersonalInformation($request, $patient);
+                break;
+            case 'contact_information':
+                return $this->__updateContactInformation($request, $patient);
+                break;
+            case 'address_information':
+                return $this->__updateAddressInformation($request, $patient);
+                break;
+            case 'diagnosis_information':
+                return $this->__updateDiagnosisInformation($request, $patient);
+                break;
+            default:
+                return response()->json(['message' => 'Page not found.']);
                 break;
         }
     }
@@ -108,6 +122,97 @@ class PatientController extends Controller
             return response()->json($returnData, 500);
         }
 
+    }
+    public function __updateContactInformation($request, $patient)
+    {
+        $request->validate([
+            'relative_name' => 'required',
+            'relation_with_relative' => 'required',
+            'contact_number_1' => 'required',
+            'contact_number_2' => 'required',
+            'email_address' => 'required',
+        ]);
+        try{
+            $patient->relative_name = $request->relative_name;
+            $patient->relation_with_relative = $request->relation_with_relative;
+            $patient->contact_number_1 = $request->contact_number_1;
+            $patient->contact_number_2 = $request->contact_number_2;
+            $patient->email_address = $request->email_address;
+            $patient->save();
+
+            $returnData = $this->prepareResponse(false, 'Success <br> Patient updated successfully.', [], []);
+            return response()->json($returnData);
+        }catch (\Exception $exception){
+            $message = $exception->getMessage();
+            $returnData = $this->prepareResponse(true, "Fail <br> $message", [], []);
+            return response()->json($returnData, 500);
+        }
+    }
+    public function __updateAddressInformation($request,$patient){
+        $request->validate([
+            'permanent_province_id' => 'required',
+            'permanent_district_id' => 'required',
+            'permanent_municipality_id' => 'required',
+            'permanent_ward' => 'required',
+            'permanent_tole' => 'required',
+            'current_province_id' => 'required',
+            'current_district_id' => 'required',
+            'current_municipality_id' => 'required',
+            'current_ward' => 'required',
+            'current_tole' => 'required',
+        ]);
+
+        try{
+            $patient->permanent_province_id = $request->permanent_province_id;
+            $patient->permanent_district_id = $request->permanent_district_id;
+            $patient->permanent_municipality_id = $request->permanent_municipality_id;
+            $patient->permanent_ward = $request->permanent_ward;
+            $patient->permanent_tole = $request->permanent_tole;
+            $patient->current_province_id = $request->current_province_id;
+            $patient->current_district_id = $request->current_district_id;
+            $patient->current_municipality_id = $request->current_municipality_id;
+            $patient->current_ward = $request->current_ward;
+            $patient->current_tole = $request->current_tole;
+            $patient->save();
+
+            $returnData = $this->prepareResponse(false, 'Success <br> Patient updated successfully.', [], []);
+            return response()->json($returnData);
+        }catch (\Exception $exception){
+            $message = $exception->getMessage();
+            $returnData = $this->prepareResponse(true, "Fail <br> $message", [], []);
+            return response()->json($returnData, 500);
+        }
+
+    }
+    public function __updateDiagnosisInformation($request, $patient){
+        $request->validate([
+            'letter_number' => 'required',
+            'letter_date' => 'required',
+            'opd_number' => 'required',
+            'hospital_bipanna_number' => 'required',
+            'disease' => 'required',
+            'max_facilitatory_amount' => 'required',
+            'referred_by' => 'required',
+            'transplant_type' => 'required',
+        ]);
+        try{
+            $patient->letter_number = $request->letter_number;
+            $patient->letter_date = $request->letter_date;
+            $patient->opd_number = $request->opd_number;
+            $patient->hospital_bipanna_number = $request->hospital_bipanna_number;
+            $patient->disease = $request->disease;
+            $patient->max_facilitatory_amount = $request->max_facilitatory_amount;
+            $patient->referred_by = $request->referred_by;
+            $patient->transplant_type = $request->transplant_type;
+            $patient->save();
+
+            $returnData = $this->prepareResponse(false, 'Success <br> Patient updated successfully.', [], []);
+            return response()->json($returnData);
+        }catch (\Exception $exception){
+            $message = $exception->getMessage();
+            $returnData = $this->prepareResponse(true, "Fail <br> $message", [], []);
+            return response()->json($returnData, 500);
+        }
     }
 
     private function checkPatient(Request $request)

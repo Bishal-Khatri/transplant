7fce79fd81fff485049179c4b59a67608b8f9342
prolaxu <adhikarilaxu0001@gmail.com>
2022-05-26 14:57:23 +0545

    Update Patient.php

--- Modules/Hospital/Entities/Patient.php
+++ Modules/Hospital/Entities/Patient.php
@@ -4,6 +4,13 @@ namespace Modules\Hospital\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Modules\Administrator\Entities\EducationLevel;
+use Modules\Administrator\Entities\EthnicGroup;
+use Modules\Administrator\Entities\Occupation;
+use Modules\Administrator\Entities\Religion;
+use Modules\Administrator\Entities\Province;
+use Modules\Administrator\Entities\District;
+use Modules\Administrator\Entities\Municipality;
 
 class Patient extends Model
 {
@@ -19,5 +26,49 @@ class Patient extends Model
     public function getCreatedAtAttribute($value)
     {
         return date('M d Y', strtotime($value));
+    }
+    // occupation
+    public function occupation()
+    {
+        return $this->belongsTo(Occupation::class);
+    }
+    // education level
+    public function education_level()
+    {
+        return $this->belongsTo(EducationLevel::class);
+    }
+    // religion
+    public function religion()
+    {
+        return $this->belongsTo(Religion::class);
+    }
+    //ethnic group
+    public function ethnic_group(){
+        return $this->belongsTo(EthnicGroup::class);
+    }
+
+    // current province
+    public function current_province(){
+        return $this->belongsTo(Province::class);
+    }
+    // current district
+    public function current_district(){
+        return $this->belongsTo(District::class);
+    }
+    // current municipality
+    public function current_municipality(){
+        return $this->belongsTo(Municipality::class);
+    }
+    // permanent province
+    public function permanent_province(){
+        return $this->belongsTo(Province::class);
+    }
+    // permanent district
+    public function permanent_district(){
+        return $this->belongsTo(District::class);
+    }
+    // permanent municipality
+    public function permanent_municipality(){
+        return $this->belongsTo(Municipality::class);
     }
 }

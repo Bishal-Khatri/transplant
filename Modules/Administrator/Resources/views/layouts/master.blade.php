1e9a735dd9c004c5507b78ac078a96dcd2455028
Bishal Khatri <bishal.khatri343@gmail.com>
2022-07-27 15:32:25 +0545

    branch merge

--- Modules/Administrator/Resources/views/layouts/master.blade.php
+++ Modules/Administrator/Resources/views/layouts/master.blade.php
@@ -4,10 +4,10 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Module Administrator</title>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>{{ config('app.name') }} - Administrator</title>
 
        {{-- Laravel Mix - CSS File --}}
-       {{-- <link rel="stylesheet" href="{{ mix('css/administrator.css') }}"> --}}
         @include('layouts.gentelella.css')
     </head>
     <body class="nav-md">
@@ -24,12 +24,7 @@
             <!-- /page content -->
 
             <!-- footer content -->
-            <footer>
-                <div class="pull-right">
-                    Bootstrap Admin Template
-                </div>
-                <div class="clearfix"></div>
-            </footer>
+        @include('layouts.gentelella.footer')
             <!-- /footer content -->
         </div>
     </div>
